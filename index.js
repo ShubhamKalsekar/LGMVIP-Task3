f2b394805b5b861b5bc4fca17d3f34b8abad5e98
Shubham Kalsekar <93903997+ShubhamKalsekar@users.noreply.github.com>
2022-10-30 13:56:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,86 @@
+
+var selectedRow = null;
+
+function onFormSubmit(e) {
+  event.preventDefault();
+  var formData = readFormData();
+  if (selectedRow == null) {
+    insertNewRecord(formData);
+  } else {
+    updateRecord(formData);
+  }
+  resetForm();
+}
+
+function readFormData() {
+  var formData = {};
+  formData["Gender"] = document.getElementById("Gender").value;
+  formData["studentName"] = document.getElementById("studentName").value;
+  formData["collegeName"] = document.getElementById("collegeName").value;
+  formData["Email"] = document.getElementById("Email").value;
+  // formData["ContactNumber"] = document.getElementById("ContactNumber").value;
+  formData["Skills"] = document.getElementById("Skills").value;
+
+  return formData;
+}
+
+function insertNewRecord(data) {
+  var table = document
+    .getElementById("storeList")
+    .getElementsByTagName("tbody")[0];
+  var newRow = table.insertRow(table.length);
+  cell1 = newRow.insertCell(0);
+  cell1.innerHTML = data.Gender;
+  cell2 = newRow.insertCell(1);
+  cell2.innerHTML = data.studentName;
+  cell3 = newRow.insertCell(2);
+  cell3.innerHTML = data.collegeName;
+  cell4 = newRow.insertCell(3);
+  cell4.innerHTML = data.Email;
+  // cell5 = newRow.insertCell(4);
+  // cell5.innerHTML = data.ContactNumber;
+  cell5 = newRow.insertCell(4);
+  cell5.innerHTML = data.Skills;
+  cell5 = newRow.insertCell(5);
+  cell5.innerHTML = `<button onClick="onEdit(this)">Edit</button> 
+                    <button onClick="onDelete(this)">Delete</button>;`
+}
+
+function onEdit(td) {
+  selectedRow = td.parentElement.parentElement;
+  document.getElementById("Gender").value = selectedRow.cells[0].innerHTML;
+  document.getElementById("studentName").value = selectedRow.cells[1].innerHTML;
+  document.getElementById("collegeName").value = selectedRow.cells[2].innerHTML;
+  document.getElementById("Email").value = selectedRow.cells[3].innerHTML;
+  // document.getElementById("ContactNumber").value = selectedRow.cells[4].innerHTML;
+  document.getElementById("Skills").value = selectedRow.cells[4].innerHTML;
+
+}
+function updateRecord(formData) {
+  selectedRow.cells[0].innerHTML = formData.Gender;  
+  selectedRow.cells[1].innerHTML = formData.studentName;
+  selectedRow.cells[2].innerHTML = formData.collegeName;
+  selectedRow.cells[3].innerHTML = formData.Email;
+  // selectedRow.cells[4].innerHTML = formData.ContactNumber;
+  selectedRow.cells[4].innerHTML = formData.Skills;
+
+}
+
+function onDelete(td) {
+  if (confirm("Do you want to delete this record?")) {
+    row = td.parentElement.parentElement;
+    document.getElementById("storeList").deleteRow(row.rowIndex);
+    resetForm();
+  }
+}
+
+function resetForm() {
+  document.getElementById("Gender").value = "";
+  document.getElementById("studentName").value = "";
+  document.getElementById("collegeName").value = "";
+  document.getElementById("Email").value = "";
+  // document.getElementById("ContactNumber").value = "";
+  document.getElementById("Skills").value = "";
+
+  selectedRow = null;
+}
